172710a102e1064c4827ec23dea27f2219a4693d
janupasilva2005 <janupasilva@gmail.com>
2022-10-17 06:20:30 +0530

    Added a new svg button that redirects you to home page

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+/**
+ * Making the new button hidden on initial load
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("new").style.display = "none";
+});
+
 var i = 0;
 var src = "hai";
 document.getElementById("viewBtn").onclick = function (e) {
-    i = 1;
-    var mainh = document.getElementById("MainHeading").value,
-      subh = document.getElementById("SubHeading").value;
-    if (mainh == "" || subh == "") {
-      alert("Please enter both headings");
-    } else {
-      src =
-        "https://svg-banners.vercel.app/api?type=origin&text1=" +
-        mainh +
-        "%20&text2=" +
-        subh +
-        "&width=1130&height=600";
-      svg = document.getElementById("svg");
-      svg.src = src;
-      document.getElementById("download").setAttribute("href", src);
-      document.getElementById("download").removeAttribute("hidden");
-      document.getElementById("viewBtn").style.display = "none";
-    }
+  i = 1;
+  var mainh = document.getElementById("MainHeading").value,
+    subh = document.getElementById("SubHeading").value;
+  if (mainh == "" || subh == "") {
+    alert("Please enter both headings");
+  } else {
+    src =
+      "https://svg-banners.vercel.app/api?type=origin&text1=" +
+      mainh +
+      "%20&text2=" +
+      subh +
+      "&width=1130&height=600";
+    svg = document.getElementById("svg");
+    svg.src = src;
+    document.getElementById("download").setAttribute("href", src);
+    document.getElementById("download").removeAttribute("hidden");
+    document.getElementById("viewBtn").style.display = "none";
+
+    document.getElementById("new").style.display = "inline-block";
+  }
 };
