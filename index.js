c2b12f7e271a3367c01407c6915eb30d57557c19
janupasilva2005 <janupasilva@gmail.com>
2022-10-17 11:39:36 +0530

    Added form validation and some comments

--- index.js
+++ index.js
@@ -5,21 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("new").style.display = "none";
 });
 
+// Grabbing the input fields
+const inputs = document.getElementsByClassName("fields");
 var i = 0;
 var src = "hai";
+
+/**
+ * View button event listener
+ */
 document.getElementById("viewBtn").onclick = function (e) {
   i = 1;
-  var mainh = document.getElementById("MainHeading").value,
-    subh = document.getElementById("SubHeading").value;
+
+  var mainh = document.getElementById("MainHeading").value; // Main heading
+  var subh = document.getElementById("SubHeading").value; // Sub heading
+
+  // Form validation
   if (mainh == "" || subh == "") {
-    alert("Please enter both headings");
+    // Making the borders red when invalid
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].style.border = "1px solid red";
+    }
   } else {
+    // Making the borders blue when valid
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].style.border = "1px solid blue";
+    }
+
     src =
       "https://svg-banners.vercel.app/api?type=origin&text1=" +
       mainh +
       "%20&text2=" +
       subh +
       "&width=1130&height=600";
+
     svg = document.getElementById("svg");
     svg.src = src;
     document.getElementById("download").setAttribute("href", src);
